ecbc8c0a7e9ff7de07c84759514568fd53797817
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2023-01-15 00:08:33 +0330

    0

--- aliases.bash
+++ aliases.bash
@@ -1,67 +1,153 @@
-#!/bin/bash
+# install -> i
+alias iftpserver='sudo apt install vsftpd'
+alias isshserver='sudo apt-get install openssh-server'
+alias ierd='sudo apt install graphviz && apt install graphviz-dev && pipenv install pyparsing pydot && pipenv install django-extensions'
 
-# app install
-alias ierd="apt install graphviz && apt install graphviz-dev && pipenv install pyparsing pydot && pipenv install django-extensions"
+# notify
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-# dbms
-alias dbmsmysqlup="systemctl start mysql.service"
+# alias
+alias alias-f="declare"
+alias alias-F="declare -F"
 
-# git
-alias ga="git add ."
-alias gc="git commit -m "
-alias gcc="git commit --amend -m "
-alias g="ga && gc"
-alias gg="ga && gcc"               # arg1: commit_msg
-alias gp="git push"
-alias gpull="git pull"
-alias gl="git log"
-alias gl1="git log --oneline"
-alias glt="git log --branches --remotes --tags --graph --oneline --decorate"
-alias glr="git remote -v"
-alias gr1="git reset --hard HEAD~1"
-alias gb="git checkout "           # arg1: branch_name
-alias gbf="git checkout -b"        # arg1: new_branch_name, arg2: hash_name 0f this branch
-alias gbv="git branch -v"          # show all local branche names 
-alias gbva="git branch -a"         # show all branche
-alias gbd="git branch -d "         # arg1: branch_name
-alias gbD="git branch -D "         # arg1: branch_name
-alias gbds="git push -d "          # arg1: server_name(example:origin) arg2: branch_name_on_server
-alias grs="git reset --soft "      # arg1: hash_name
-alias grh="git reset --hard "      # arg1: hash_name
-alias gt="ssh -T git@gitlab.com"
-gmerge() {
-    echo $(grh $1 && gcc $2) 
+# grep
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# list
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+
+# link
+alias hln="ln" # hln src dst
+alias sln="ln -s" # sln src dst
+
+# filesystem
+alias mkp='mkdir -p'
+
+# ps
+alias ps1='ps -p 1 -o comm='
+
+# user -> u
+alias uadd='sudo useradd -m '
+
+# systemctl -> sc
+alias sc='sudo systemctl'
+alias scs='sudo systemctl status '
+alias scd='sudo systemctl disable '
+alias sce='sudo systemctl enable '
+alias scr='sudo systemctl restart '
+alias scstart='sudo systemctl start '
+alias scstop='sudo systemctl stop '
+alias scfSD='sudo systemctl --type=service --state=dead'
+alias scfSE='sudo systemctl --type=service --state=exited'
+alias scfSF='sudo systemctl --type=service --state=failed'
+alias scfSI='sudo systemctl --type=service --state=inactive'
+alias scfSR='sudo systemctl --type=service --state=running'
+
+# service -> ss
+alias ssrftp='ssr vsftpd'
+ssr () 
+{ 
+    echo $(sudo service $1 restart)
+}
+sss () 
+{ 
+    echo $(sudo service $1 status)
+}
+ssstart () 
+{ 
+    echo $(sudo service $1 start)
+}
+ssstop () 
+{ 
+    echo $(sudo service $1 stop)
 }
 
-# django basic
-alias pm="python manage.py"
-alias pmapp="python manage.py startapp " # arg1: app_name
-alias pmcs="python manage.py collectstatic --noinput"
-alias pmdbsh="python manage.py dbshell"
-alias pmdd="python manage.py dumpdata"
-alias pmld="python manage.py loaddata"
-alias pmm="python manage.py migrate"
-alias pm2m="python manage.py makemigrations"
-alias pmmg="pm2m && python manage.py migrate"
-alias pmsh="python manage.py shell"
-alias pmsm="python manage.py schemamigration"
-alias pmsync="python manage.py syncdb --noinput"
-alias pmt="python manage.py test"
-alias pmu="python manage.py createsuperuser"
-alias pmurp="python manage.py changepassword " # arg1: username
-alias pmrs="python manage.py runserver"
-alias pmsm="python manage.py sqlmigrate auth 0001" # TODO: try to define a function for this
-pmsql() {
-    echo $(python manage.py makemigrations $1 --empty) 
-}
-pmseed() {
-    echo $(python manage.py seed $1 --number=$2) 
+# firewall -> fw
+alias fwcp='sudo ufw deny '
+alias fwop='sudo ufw allow '
+
+# ftp
+alias ftph='ftp $(hostname)'
+alias scstartftp='scstart vsftpd'
+alias sceftp='sce vsftpd'
+alias scrftp='scr vsftpd'
+
+# ssh
+alias scstartssh='scstart ssh'
+alias scessh='sce ssh'
+
+# mysql
+alias scstartmysql='scstart mysql.service'
+
+# git -> g | Reserved -> alias gt='ssh -T git@gitlab.com'
+alias g='ga && gc'
+alias ga='git add .'
+alias gb='git checkout '
+alias gbD='git branch -D '
+alias gbd='git branch -d '
+alias gbds='git push -d '
+alias gbf='git checkout -b'
+alias gbv='git branch -v'
+alias gbva='git branch -a'
+alias gc='git commit -m '
+alias gcc='git commit --amend -m '
+alias gclear='git rm -r --cached '
+alias gg='ga && gcc'
+alias gl='git log'
+alias gl1='git log --oneline'
+alias glr='git remote -v'
+alias glt='git log --branches --remotes --tags --graph --oneline --decorate'
+alias gp='git push'
+alias gpull='git pull'
+alias gr1='git reset --hard HEAD~1'
+alias grh='git reset --hard '
+alias grs='git reset --soft '
+gmerge () 
+{ 
+    echo $(grh $1 && gcc $2)
 }
 
-# os
-alias mkp="mkdir -p"
+# django(python manage) -> pm
+alias pm='python manage.py'
+alias pm2m='python manage.py makemigrations'
+alias pmapp='python manage.py startapp '
+alias pmcs='python manage.py collectstatic --noinput'
+alias pmdbsh='python manage.py dbshell'
+alias pmdd='python manage.py dumpdata'
+alias pmerddot='python manage.py graph_models -a > documentation/ERD.dot'
+alias pmerd='pmerddot && python manage.py graph_models --pydot -a -g -o documentation/ERD.png'
+alias doc='mkp documentation && pmerd'
+alias pmld='python manage.py loaddata'
+alias pmm='python manage.py migrate'
+alias pmmg='pm2m && python manage.py migrate'
+alias pmrs='python manage.py runserver'
+alias pmsh='python manage.py shell'
+alias pmsm='python manage.py sqlmigrate auth 0001'
+alias pmsync='python manage.py syncdb --noinput'
+alias pmt='python manage.py test'
+alias pmu='python manage.py createsuperuser'
+alias pmurp='python manage.py changepassword '
+pmseed () 
+{ 
+    echo $(python manage.py seed $1 --number=$2)
+}
+pmsql () 
+{ 
+    echo $(python manage.py makemigrations $1 --empty)
+}
 
-# documentation
-alias pmerddot="python manage.py graph_models -a > documentation/ERD.dot"
-alias pmerd="pmerddot && python manage.py graph_models --pydot -a -g -o documentation/ERD.png"
-alias doc="mkp documentation && pmerd"
+
+# temp
+alias tempopenportftp='sudo ufw allow 20/tcp && sudo ufw allow 21/tcp'
+
+# task -> t
+alias tftpserver='iftpserver && scstartftp && sceftp && tempopenportftp '
+alias tsshserver='isshserver && scessh && scstartssh'
+
+
+
